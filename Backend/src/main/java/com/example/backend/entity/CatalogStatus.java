563e8c7a75bf801dcd19626fdb8785761cd6eb9c
Nethsara <Nethsara@gmail.com>
2026-02-14 22:40:27 +0530

    updated

--- Backend/src/main/java/com/example/backend/entity/CatalogStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.backend.entity;
-
-public enum CatalogStatus {
-    ACTIVE,
-    ARCHIVED
-}
--- Backend/src/main/java/com/example/backend/entity/PharmacyStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.entity;
-
-public enum PharmacyStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
